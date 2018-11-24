07b601504ebdbb7d145013d7ce63589f2953a206
unknown <trevorsiow@gmail.com>
2018-11-24 16:43:47 -0500

    created matlab script to solve fibonacci sequence. Have not implemented timer feature

---
+++ fibonacci.m
@@ -0,0 +1,12 @@
+function [fibValue] = fibonacci(nth)
+%Return the nth fibonacci number
+
+previousValue = 0;
+fibValue = 1;
+for i = 0:nth - 1   %inclusive both bounds
+    temp = fibValue
+    fibValue = fibValue + previousValue
+    previousValue = temp
+end
+end
+
